551ae3bc7bc19301382bb345ad50c4c7608ac0d5
diarmuid-io <58391277+diarmuid-io@users.noreply.github.com>
2020-10-14 15:39:54 +0200

    Updates

---
+++ precache-manifest.275ee164bc7d41c06caeb49ce844e602.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999a420873c81ef32f0d2abe603adf86",
+    "url": "/robofriends-react-app/index.html"
+  },
+  {
+    "revision": "134f4ba24a57884c5e2c",
+    "url": "/robofriends-react-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8a9afacae358108a13ef",
+    "url": "/robofriends-react-app/static/css/main.b3d02026.chunk.css"
+  },
+  {
+    "revision": "134f4ba24a57884c5e2c",
+    "url": "/robofriends-react-app/static/js/2.e4cf57e9.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robofriends-react-app/static/js/2.e4cf57e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9afacae358108a13ef",
+    "url": "/robofriends-react-app/static/js/main.5435c6af.chunk.js"
+  },
+  {
+    "revision": "a22466a56537f0fdf8f7",
+    "url": "/robofriends-react-app/static/js/runtime-main.8acdbbdc.js"
+  },
+  {
+    "revision": "0f9bdc30d3db526b02072d19a12cd032",
+    "url": "/robofriends-react-app/static/media/Blanka-Regular.0f9bdc30.otf"
+  }
+]);
